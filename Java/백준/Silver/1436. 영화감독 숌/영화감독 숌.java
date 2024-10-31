3baac289aac561b875fee336c116efa7a613a25a
minjun9-9 <127954358+minjun9-9@users.noreply.github.com>
2024-10-31 20:46:23 +0900

    [Silver V] Title: 영화감독 숌, Time: 360 ms, Memory: 88736 KB -BaekjoonHub

---
+++ Java/백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int num = 666;
+		int count = 1;
+		while(count != n) {
+			num++;
+			if(String.valueOf(num).contains("666")) {
+				count++;
+			}
+		}
+		System.out.println(num);
+	}
+
+}
