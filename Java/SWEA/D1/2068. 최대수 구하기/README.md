bc8a3816cd3c3db8f35da750b8dba72e6f747d21
minjun9-9 <127954358+minjun9-9@users.noreply.github.com>
2025-11-14 00:25:48 +0900

    [D1] Title: 최대수 구하기, Time: 75 ms, Memory: 22,144 KB -BaekjoonHub

---
+++ Java/SWEA/D1/2068. 최대수 구하기/README.md
@@ -0,0 +1,15 @@
+# [D1] 최대수 구하기 - 2068 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QQhbqA4QDFAUq) 
+
+### 성능 요약
+
+메모리: 22,144 KB, 시간: 75 ms, 코드길이: 2,353 Bytes
+
+### 제출 일자
+
+2025-11-14 00:25
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ Java/SWEA/D1/2068. 최대수 구하기/최대수 구하기.java
@@ -0,0 +1,60 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{	
+    static int max = 0;
+	public static void main(String args[]) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int a = Integer.parseInt(br.readLine().trim());
+		StringTokenizer st;
+		int num = 1;
+		for(int i = 0; i < a; i++) {
+			max = 0;
+			String abc = br.readLine();
+			while(abc != null && abc.trim().isEmpty()) {
+				abc = br.readLine();
+			}
+			if(abc==null)break;
+			st = new StringTokenizer(abc);
+			for(int j = 0; j < 10; j++) {
+				int arr = Integer.parseInt(st.nextToken());
+				if(max < arr)max=arr;
+			}
+			System.out.println("#"+num++ + " "+ max);
+		}
+	}
+}
