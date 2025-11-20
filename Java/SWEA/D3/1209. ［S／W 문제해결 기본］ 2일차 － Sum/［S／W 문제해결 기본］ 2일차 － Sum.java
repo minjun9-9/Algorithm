b206d4c1cda26982f092bea8dd131cc9c46ca402
minjun9-9 <127954358+minjun9-9@users.noreply.github.com>
2025-11-20 17:31:58 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 126 ms, Memory: 36,592 KB -BaekjoonHub

---
+++ Java/SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
@@ -0,0 +1,74 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = 10,SIZE=100;
+        int[][] map;
+        
+		for(int tc = 1; tc <= T; tc++)
+		{
+            br.readLine();
+            map = new int[SIZE][SIZE];
+            StringTokenizer st;
+            for(int i = 0; i < SIZE; i++){
+                st = new StringTokenizer(br.readLine());
+                for(int j = 0; j < SIZE; j++){
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+            int result = 0,crossR = 0, crossL = 0;
+            for(int i = 0; i < SIZE ; i++){
+                int row = 0,col = 0;
+                for(int j = 0; j < SIZE; j++){
+                    row += map[i][j];
+                    col += map[j][i];
+                    if(i == j){
+                        crossR += map[i][j];
+                        crossL += map[i][SIZE - j - 1];
+                    }
+                }
+                result = Math.max(result, Math.max(row,col));
+            }
+            result = Math.max(result, Math.max(crossR,crossL));
+            System.out.println("#"+tc+" " + result);
+		}
+	}
+}
