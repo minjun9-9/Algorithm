e532757021e06ce6ca0ef81dd80eedb760f0b220
minjun9-9 <127954358+minjun9-9@users.noreply.github.com>
2025-11-21 20:46:40 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 141 ms, Memory: 37,496 KB -BaekjoonHub

--- Java/SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
+++ Java/SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
@@ -40,35 +40,40 @@ class Solution
 	public static void main(String args[]) throws Exception
 	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = 10,SIZE=100;
-        int[][] map;
-        
-		for(int tc = 1; tc <= T; tc++)
-		{
-            br.readLine();
-            map = new int[SIZE][SIZE];
-            StringTokenizer st;
-            for(int i = 0; i < SIZE; i++){
-                st = new StringTokenizer(br.readLine());
-                for(int j = 0; j < SIZE; j++){
-                    map[i][j] = Integer.parseInt(st.nextToken());
-                }
-            }
-            int result = 0,crossR = 0, crossL = 0;
-            for(int i = 0; i < SIZE ; i++){
-                int row = 0,col = 0;
-                for(int j = 0; j < SIZE; j++){
-                    row += map[i][j];
-                    col += map[j][i];
-                    if(i == j){
-                        crossR += map[i][j];
-                        crossL += map[i][SIZE - j - 1];
-                    }
-                }
-                result = Math.max(result, Math.max(row,col));
-            }
-            result = Math.max(result, Math.max(crossR,crossL));
-            System.out.println("#"+tc+" " + result);
+		int tc = 10;
+		for(int t = 1; t <= 10; t++) {
+			int[][] arr = new int[100][100];
+			int res = 0;
+			br.readLine();
+			for(int i = 0;  i< 100; i++) {
+				String str = br.readLine().trim();
+				StringTokenizer st = new StringTokenizer(str);
+				for(int j = 0; j < 100; j++) {
+					arr[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			int lc = 0;
+			int rc = 0;
+			for(int i = 0; i < 100; i++) {
+				int smax = 0;
+				int hmax = 0;
+				int tmax = 0;
+				for(int j = 0; j < 100; j++) {
+					smax += arr[i][j];
+					hmax += arr[j][i];
+				}
+				tmax = Math.max(smax, hmax);
+				
+				if(res <= tmax) res = tmax;
+				lc += arr[i][i];
+				rc += arr[i][99-i];
+			}
+			res = Math.max(res, Math.max(lc, rc));
+			
+			
+			
+			
+			System.out.println("#"+t +" "+ res);
 		}
 	}
 }
